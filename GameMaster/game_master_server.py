196526cfb34cb37bdf04ec83bb5747734b342fd8
Kallinteris Andreas <kallinteris@protonmail.com>
2021-05-01 20:08:15 +0300

    fix bug

--- GameMaster/game_master_server.py
+++ GameMaster/game_master_server.py
@@ -7,6 +7,8 @@ import random
 import game_master_db
 import game_master_mongo_interface
 
+db_lock = threading.Lock()
+
 practice_chess_waiting_list = None
 practice_ttt_waiting_list = None
 practice_chess_match_queue_id = None
@@ -16,8 +18,10 @@ play_master_url = "http://playmaster:8080/"
 auth_url = "http://authmanager:42069/"
 
 def create_play(game_type, username0, username1, tournament_name):
+    db_lock.acquire()
     game_master_db.create_match(game_type, username0, username1, tournament_name)
     match_id = game_master_db.max_match_id()
+    db_lock.release()
     data = {'playId': match_id, 'player1': username0, "player2": username1, "gameType": game_type, "tournamentName" : tournament_name}
     json_data = json.dumps(data, indent = 4)
     r = requests.post(url = play_master_url + "createPlay", data = json_data)
@@ -46,14 +50,18 @@ def manage_tournament(tournament_name, game_type):
     place3 = ''#TODO
     while len(remaining_players_of_tourny[tournament_name]) != 1:
         #create matches for all the available players
-        random.shuffle(remaining_players_of_tourny[tournament_name])
+        #print("pre" + str(remaining_players_of_tourny[tournament_name]))
+        #random.shuffle(remaining_players_of_tourny[tournament_name])
+        #print("post" + str(remaining_players_of_tourny[tournament_name]))
+        assert isinstance(remaining_players_of_tourny[tournament_name], list)
         for i in range(0, int(len(remaining_players_of_tourny[tournament_name])/2)):
             match_id = create_play(game_type, remaining_players_of_tourny[tournament_name][i*2], remaining_players_of_tourny[tournament_name][(i*2)+1], tournament_name)
             remaining_matches_of_tourny[tournament_name].append(match_id)
-        if len(remaining_players_of_tourny[tournament_name]) == 3 or len(remaining_players_of_tourny[tournament_name] == 4):
+        if len(remaining_players_of_tourny[tournament_name]) == 3 or len(remaining_players_of_tourny[tournament_name]) == 4:
             semi_finals_player_list = remaining_players_of_tourny[tournament_name]
         if len(remaining_players_of_tourny[tournament_name]) > 4:
             quarter_finals_player_list = remaining_players_of_tourny[tournament_name]
+        assert isinstance(remaining_players_of_tourny[tournament_name], list)
         
         players_of_previous_round = remaining_players_of_tourny[tournament_name]
         if (len(remaining_players_of_tourny[tournament_name])%2 == 1):#if odd number of contesties left grant a bye to someone
@@ -80,7 +88,9 @@ def manage_tournament(tournament_name, game_type):
         quarter_finals_player_list.remove(place2)
         place3 = quarter_finals_player_list[0]
     
+    db_lock.acquire()
     game_master_db.finish_tournament(tournament_name, place0, place1, place2, place3)
+    db_lock.release()
     
     del remaining_matches_of_tourny[tournament_name]
     del remaining_players_of_tourny[tournament_name]
@@ -93,7 +103,9 @@ class game_master_handler(BaseHTTPRequestHandler):
         if self.path.endswith('/gameFinished'):
             finished_games = game_master_mongo_interface.extract_finished_games()
             for game in finished_games:
+                db_lock.acquire()
                 game_master_db.finish_match(game[0], game[1])
+                db_lock.release()
                 for tournament_name in remaining_matches_of_tourny:#handle case of tournament match
                     if game[0] in remaining_matches_of_tourny[tournament_name]:
                         remaining_matches_of_tourny[tournament_name].remove(game[0])
@@ -103,13 +115,17 @@ class game_master_handler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
         elif self.path.endswith('/getTournaments'):
+            db_lock.acquire()
             response = json.dumps(game_master_db.get_all_tournaments_formated(), indent=4)
+            db_lock.release()
             self.send_response(200)
             self.send_header('Content-type','application/json')
             self.end_headers()
             self.wfile.write(response.encode("utf-8"))
         if self.path.endswith('/getAllPlayers'):
+            db_lock.acquire()
             response = json.dumps(game_master_db.get_all_players_formated(), indent=4)
+            db_lock.release()
             self.send_response(200)
             self.send_header('Content-type','application/json')
             self.end_headers()
@@ -201,6 +217,7 @@ class game_master_handler(BaseHTTPRequestHandler):
             self.end_headers()
         elif self.path.endswith('/getMyScores'):
             username = (json_body['username'])
+            db_lock.acquire()
             practice_score = game_master_db.get_n_practice_wins(username)*3 + game_master_db.get_n_practice_draws(username)
             practice_plays_num = game_master_db.get_n_practice_plays(username)
             tournament_score = game_master_db.get_n_tournament_wins(username)*3 + game_master_db.get_n_tournament_draws(username)
@@ -209,6 +226,7 @@ class game_master_handler(BaseHTTPRequestHandler):
             draws = game_master_db.get_n_draws(username)
             loses = game_master_db.get_n_loses(username)
             plays = game_master_db.get_all_plays_formated(username)
+            db_lock.release()
             response = json.dumps({"practiceScore":practice_score, "practicePlaysNum":practice_plays_num, "tournamentScore":tournament_score, "tournamentPlaysNum": tournament_plays_num, "wins": wins, "ties": draws, "losses": loses, "plays": plays}, indent=4)
             self.send_response(200)
             self.send_header('Content-type','application/json')
@@ -217,11 +235,14 @@ class game_master_handler(BaseHTTPRequestHandler):
         elif self.path.endswith('/createTournament'):
             tournament_name = (json_body['tournamentName'])
             game_type = (json_body['gameType'])
+            db_lock.acquire()
             if game_master_db.check_tournament(tournament_name):
                 self.send_response(500)
                 self.end_headers()
+                db_lock.release()
                 return
             game_master_db.insert_tournament(tournament_name, game_type)
+            db_lock.release()
             threading.Thread(target=manage_tournament, args=(tournament_name, game_type,)).start()
             self.send_response(200)
             self.end_headers()
