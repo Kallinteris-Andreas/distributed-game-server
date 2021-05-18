3dffd5fd915a06b374a97ea96205456da320fc7d
John <iapostolakis3@isc.tuc.gr>
2021-05-18 18:02:04 +0300

    Moved from docker-compose to minikube

---
+++ initMiniKube.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+minikube mount .:/hostprj &
+minikube kubectl -- apply -f ./project.yaml
+echo -e "\nYou can connect with your browser to the following link once all pods have started:"
+minikube service --url userinterface
