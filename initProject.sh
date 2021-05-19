9ffef0c1fbeb8614c28a95efb90f043920eeb68c
John <iapostolakis3@isc.tuc.gr>
2021-05-19 17:06:03 +0300

    Improvements

--- initProject.sh
+++ initProject.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+minikube start
 minikube mount .:/hostprj &
 minikube kubectl -- apply -f ./project.yaml
 echo -e "\nYou can connect with your browser to the following link once all pods have started:"
